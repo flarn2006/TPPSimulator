ae264778914cf0d6df7b3ac3d2fe676b1e74fee4
flarn2006 <flarn2006@gmail.com>
2015-02-18 21:27:17 -0500

    Why didn't these files get committed?

---
+++ TPPSimulator/ResizeMapDialog.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TPPSimulator
+{
+    partial class ResizeMapDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.udWidth = new System.Windows.Forms.NumericUpDown();
+            this.udHeight = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.udWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udHeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width:";
+            // 
+            // udWidth
+            // 
+            this.udWidth.Location = new System.Drawing.Point(59, 12);
+            this.udWidth.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.udWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.udWidth.Name = "udWidth";
+            this.udWidth.Size = new System.Drawing.Size(54, 20);
+            this.udWidth.TabIndex = 1;
+            this.udWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // udHeight
+            // 
+            this.udHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.udHeight.Location = new System.Drawing.Point(59, 44);
+            this.udHeight.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.udHeight.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.udHeight.Name = "udHeight";
+            this.udHeight.Size = new System.Drawing.Size(54, 20);
+            this.udHeight.TabIndex = 3;
+            this.udHeight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Height:";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(127, 11);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(127, 42);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // ResizeMapDialog
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(214, 76);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.udHeight);
+            this.Controls.Add(this.udWidth);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ResizeMapDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Resize Map";
+            ((System.ComponentModel.ISupportInitialize)(this.udWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udHeight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown udWidth;
+        private System.Windows.Forms.NumericUpDown udHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ TPPSimulator/ResizeMapDialog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPPSimulator
+{
+    public partial class ResizeMapDialog : Form
+    {
+        private int oldWidth = 0;
+        private int oldHeight = 0;
+
+        public ResizeMapDialog()
+        {
+            InitializeComponent();
+        }
+
+        public static DialogResult PromptToResize(TileGrid tileGrid)
+        {
+            ResizeMapDialog dlg = new ResizeMapDialog();
+            dlg.udWidth.Value = dlg.oldWidth = tileGrid.Columns;
+            dlg.udHeight.Value = dlg.oldHeight = tileGrid.Rows;
+            DialogResult result = dlg.ShowDialog();
+
+            if (result == DialogResult.OK) {
+                tileGrid.ResizeGrid((int)dlg.udWidth.Value, (int)dlg.udHeight.Value);
+            }
+
+            return result;
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            bool proceed = true;
+
+            if (udWidth.Value < oldWidth || udHeight.Value < oldHeight) {
+                proceed = (MessageBox.Show("The size you chose is smaller than the current size; some clipping will occur.", "Warning",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == DialogResult.OK);
+            }
+
+            if (proceed) DialogResult = DialogResult.OK;
+        }
+    }
+}
