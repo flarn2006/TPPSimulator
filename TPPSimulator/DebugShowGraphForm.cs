33d327da0e654bd73d955fc4399896903a5b83c8
flarn2006 <flarn2006@gmail.com>
2015-02-27 23:39:05 -0500

    Debug graph display now shows predecessors

--- TPPSimulator/DebugShowGraphForm.cs
+++ TPPSimulator/DebugShowGraphForm.cs
@@ -33,7 +33,7 @@ namespace TPPSimulator
         {
             Bitmap bmp = new Bitmap(64 * (cols + 1), 64 * (rows + 1));
             Graphics g = Graphics.FromImage(bmp);
-            Pen edgePen = new Pen(Color.Lime, 8);
+            Pen edgePen = new Pen(Color.Lime, 8.0f);
             edgePen.EndCap = LineCap.ArrowAnchor;
             g.FillRectangle(Brushes.Black, new Rectangle(Point.Empty, bmp.Size));
 
@@ -43,15 +43,33 @@ namespace TPPSimulator
                 g.DrawLine(edgePen, start, end);
             }
 
-            Font distanceFont = new Font("Impact", 24.0f, FontStyle.Regular, GraphicsUnit.Point); //Fixedsys didn't work for some reason
-            Brush distanceBrush = new SolidBrush(Color.FromArgb(64, 255, 255, 255));
             foreach (Node<Point> node in graph.AllNodes) {
                 Point pt = TranslateGraphPoint(node.Data);
                 int colorValue = 16 * (int)node.PathDistance;
                 if (colorValue < 0) colorValue = 0; if (colorValue > 255) colorValue = 255;
                 Brush br = new SolidBrush(Color.FromArgb(255 - colorValue, 0, colorValue));
                 g.FillEllipse(br, pt.X - 8, pt.Y - 8, 16, 16);
-                g.DrawString((node.PathDistance == Int64.MaxValue ? "Inf" : node.PathDistance.ToString()), distanceFont, distanceBrush, pt);
+            }
+
+            Font distanceFont = new Font("Impact", 24.0f, FontStyle.Regular, GraphicsUnit.Point); //Fixedsys didn't work for some reason
+            Brush distanceBrush = new SolidBrush(Color.FromArgb(64, 255, 255, 255));
+            Pen predecessorPen = new Pen(Color.Yellow, 2.0f);
+            predecessorPen.EndCap = LineCap.Custom;
+            predecessorPen.CustomEndCap = new AdjustableArrowCap(6.0f, 8.0f);
+            foreach (Node<Point> node in graph.AllNodes) {
+                if (node.PathPredecessor != null) {
+                    Point start = TranslateGraphPoint(node.Data);
+                    Point end = TranslateGraphPoint(node.PathPredecessor.Data);
+                    Point mid;
+                    if (start.X == end.X) {
+                        mid = new Point(start.X + 16, (start.Y + end.Y) / 2);
+                    } else {
+                        mid = new Point((start.X + end.X) / 2, start.Y - 16);
+                    }
+                    g.DrawCurve(predecessorPen, new Point[] { start, mid, end }, 1.0f);
+                }
+
+                g.DrawString((node.PathDistance == Int64.MaxValue ? "Inf" : node.PathDistance.ToString()), distanceFont, distanceBrush, TranslateGraphPoint(node.Data));
             }
 
             DebugShowGraphForm form = new DebugShowGraphForm();
