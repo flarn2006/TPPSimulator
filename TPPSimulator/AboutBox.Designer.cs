984bc1417ab9d2c72d4e39b8e217ac109deba306
flarn2006 <flarn2006@gmail.com>
2015-03-01 03:27:18 -0500

    Added "Beta 2" to about box

--- TPPSimulator/AboutBox.Designer.cs
+++ TPPSimulator/AboutBox.Designer.cs
@@ -64,11 +64,11 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(44, 16);
+            this.label1.Location = new System.Drawing.Point(3, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 25);
+            this.label1.Size = new System.Drawing.Size(231, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "TPP Simulator";
+            this.label1.Text = "TPP Simulator (Beta 2)";
             // 
             // groupBox1
             // 
