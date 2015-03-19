25562fe0ab81dd4d618e50406ea5642198d7f090
flarn2006 <flarn2006@gmail.com>
2015-03-19 18:27:57 -0400

    Change about box text

--- TPPSimulator/AboutBox.Designer.cs
+++ TPPSimulator/AboutBox.Designer.cs
@@ -63,12 +63,12 @@
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(231, 25);
+            this.label1.Size = new System.Drawing.Size(214, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "TPP Simulator (Beta 3)";
+            this.label1.Text = "TPP Simulator (TwitchSpeaks Edition)";
             // 
             // groupBox1
             // 
