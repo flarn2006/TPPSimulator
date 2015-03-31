a7ebd1415f80861d7b14666c304dca2f57f6e076
flarn2006 <flarn2006@gmail.com>
2015-03-30 20:06:07 -0400

    Prevented pastebin import dialog from showing in the taskbar

---
+++ TPPSimulator/PastebinImportForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TPPSimulator
+{
+    partial class PastebinImportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a Pastebin URL:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtURL.Location = new System.Drawing.Point(12, 25);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(292, 20);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.TextChanged += new System.EventHandler(this.txtURL_TextChanged);
+            this.txtURL.Enter += new System.EventHandler(this.txtURL_Enter);
+            this.txtURL.Validating += new System.ComponentModel.CancelEventHandler(this.txtURL_Validating);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(229, 61);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Enabled = false;
+            this.btnOK.Location = new System.Drawing.Point(148, 61);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 66);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Status";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // PastebinImportForm
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(316, 96);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "PastebinImportForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Import from Pastebin";
+            this.Load += new System.EventHandler(this.PastebinImportForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ TPPSimulator/PastebinImportForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TPPSimulator
+{
+    public partial class PastebinImportForm : Form
+    {
+        private Exception downloadError = null;
+        private string filename = null;
+
+        public PastebinImportForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void PastebinImportForm_Load(object sender, EventArgs e)
+        {
+            string clipboard = Clipboard.GetText();
+            txtURL.SelectAll();
+            lblStatus.Text = "";
+            if (clipboard.StartsWith("http://")) {
+                txtURL.Text = clipboard;
+                await DownloadMap();
+            }
+        }
+
+        public async Task DownloadMap()
+        {
+            Match match = Regex.Match(txtURL.Text, "^(?:http://)?(?:www.)?pastebin.com/([A-Za-z0-9]+)$");
+            if (match.Success) {
+                txtURL.Text = "http://pastebin.com/raw.php?i=" + match.Groups[1].Value;
+            }
+
+            HttpClient client = new HttpClient();
+            client.Timeout = new TimeSpan(0, 0, 5);
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, txtURL.Text);
+
+            lblStatus.ForeColor = Color.Olive;
+            lblStatus.Text = "Downloading...";
+            downloadError = null;
+            lblStatus.Cursor = Cursors.Default;
+            btnOK.Enabled = false;
+
+            try {
+                HttpResponseMessage resp = await client.SendAsync(req);
+                if (resp.Content.Headers.ContentType.MediaType.Equals("text/plain")) {
+                    byte[] content = await resp.Content.ReadAsByteArrayAsync();
+                    filename = Path.Combine(Path.GetTempPath(), "Imported.tppmap");
+                    FileStream file = File.OpenWrite(filename);
+                    file.Write(content, 0, content.Length);
+                    file.Close();
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Text = "Downloaded";
+                    btnOK.Enabled = true;
+                    btnOK.Focus();
+                } else {
+                    lblStatus.ForeColor = Color.Red;
+                    lblStatus.Text = "Wrong content type";
+                }
+            } catch (InvalidOperationException) {
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = "Invalid URL";
+            } catch (Exception ex) {
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = "Error, click for more info";
+                downloadError = ex;
+                lblStatus.Cursor = Cursors.Hand;
+            }
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+            if (downloadError != null) {
+                string message = downloadError.GetType().FullName + ":\r\n" + downloadError.Message;
+                MessageBox.Show(message, "Error Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public static DialogResult ImportMap(out string filename)
+        {
+            PastebinImportForm form = new PastebinImportForm();
+            DialogResult result = form.ShowDialog();
+            if (result == DialogResult.OK) {
+                filename = form.filename;
+            } else {
+                filename = null;
+            }
+            return result;
+        }
+
+        private void txtURL_Enter(object sender, EventArgs e)
+        {
+            txtURL.SelectAll();
+        }
+
+        private void txtURL_TextChanged(object sender, EventArgs e)
+        {
+            lblStatus.Text = "";
+            downloadError = null;
+            btnOK.Enabled = false;
+        }
+
+        private async void txtURL_Validating(object sender, CancelEventArgs e)
+        {
+            if (!btnOK.Enabled) {
+                await DownloadMap();
+            }
+        }
+    }
+}
